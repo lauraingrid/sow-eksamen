85ba7557769eb0061a83d511c4ab0f38a74c8ba2
lauraingrid <laura.weile@outlook.dk>
2024-05-30 12:13:34 +0200

    slettet ubrugte sider

--- public/scripts/scrollknap.js
+++ public/scripts/scrollknap.js
@@ -12,9 +12,3 @@ function scrollFunction() {
     scrollToTopBtn.style.display = "none";
   }
 }
-
-// Scroll to the top when the button is clicked
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-}
